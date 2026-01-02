15b68a16325c5db1862f25c6a6c7dd377bd95333
Harshit goel <137608831+Harshitgoel12@users.noreply.github.com>
2026-01-02 12:11:53 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 4000;
 // Connect DB + Cloudinary
 connectDB()
   .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err.message));
+  .catch((err) => console.error(" MongoDB connection error:", err.message));
 
 connectCloudinary();
 
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",").map((o) => o.trim())
-  : [process.env.ORIGIN, "https://e-commerce-yadr.vercel.app","https://localhost:5173"];
+  : [process.env.ORIGIN, "https://e-commerce-yadr.vercel.app","https://localhost:5173","https://e-commerce-zeta-tan.vercel.app/"."*"];
 
 app.use(
   cors({
